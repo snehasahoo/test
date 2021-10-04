cab5d3fc1bcfd2718a8c99ee668ebb66e7e88335
Sneha Sahoo <73032794+snehasahoo@users.noreply.github.com>
2021-10-04 20:56:51 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,1 @@
 import pygame
-print("2 Types of operations can be performed over a strings:")
-print()
-print("1) Repetition(*): example we can write 'i' 5 times like","i"*5)
-print("2) Concatenation(+): example we can write Hello, World! like","Hello, "+"World!")
